c4cab4954307bf91123f145410b888cbc4b214c3
quinnium <78171763+quinnium@users.noreply.github.com>
2023-08-21 13:26:55 +0100

    Initial Commit

---
+++ MessagingExercise/MessagingExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessagingExerciseApp.swift
+//  MessagingExercise
+//
+//  Created by Quinn on 21/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MessagingExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
